532368bc8ffee6d88509bd67059621f4464a700d
Joshua McClary <joshuamcclary@u.boisestate.edu>
2021-11-10 13:58:32 -0700

    Boss AI State Machine

---
+++ Assets/Scripts/AI State Machine/AIControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    GameObject player;
+    
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AI State Machine/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
