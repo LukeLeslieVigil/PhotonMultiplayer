63573a7286c24382ab5327bbbec45585234a505d
LukeLeslieVigil <seventaco123@gmail.com>
2021-11-09 17:06:42 -0700

    New Player Controller and Gun setup

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        //Checks if the view is the local player then creates a controller
+        if (PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void CreateController()
+    {
+        Debug.Log("CreateController Instantiate");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
+    }
+}
