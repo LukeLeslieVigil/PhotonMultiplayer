cab315bcd5443ee08c2c392702336c3682affd46
Joshua McClary <joshuamcclary@u.boisestate.edu>
2021-11-14 00:46:12 -0700

    Prevented Total Disaster

---
+++ Assets/Scripts/BossManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    GameObject player;
+
+    public GameObject boss;
+
+    int bossCount = 0;
+
+    private void Update()
+    {
+        CheckForPlayer();
+    }
+
+    void CheckForPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            if (bossCount <= 0)
+            {
+                Instantiate(boss);
+                bossCount++;
+            }
+        }
+    }
+}
