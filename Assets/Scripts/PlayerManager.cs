22f1904894e4c9566aa08159e93e4e9bc6938a13
Joshua McClary <joshuamcclary@u.boisestate.edu>
2021-11-14 00:25:28 -0700

    New PlayerController With Boss

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -273,3 +274,44 @@ namespace Com.DMO.Multiplayer
         #endregion
     }
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    PhotonView PV;
+    GameObject controller;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        //Checks if the view is the local player then creates a controller
+        if (PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Transform spawnpoint = SpawnManager.Instance.GetSpawnPoint();
+        Debug.Log("CreateController Instantiate");
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
+    }
+
+    public void Die()
+    {
+        PhotonNetwork.Destroy(controller);
+        CreateController();
+    }
+}
+>>>>>>> Stashed changes
