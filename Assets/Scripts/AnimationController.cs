35ebbcf740a5b03875a7fe769ea868c77fc39e67
LukeLeslieVigil <seventaco123@gmail.com>
2021-11-16 22:29:36 -0700

    Some animations

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+    float currentSpeed;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        currentSpeed = animator.GetCurrentAnimatorStateInfo(0).speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAnimationState();
+    }
+
+    void CheckAnimationState()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetTrigger("Walk");
+            animator.ResetTrigger("Idle");
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetTrigger("Run");
+                animator.ResetTrigger("Walk");
+            }
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+            animator.ResetTrigger("Run");
+        }
+    }
+}
